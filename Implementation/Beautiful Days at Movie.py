e42a53a5b9cf5a0a8dfc106f1f0bd2ba3d73e346
Aadityaganesh <adityaganesh234@gmail.com>
2024-11-21 18:29:33 +0530

    Create Beautiful Days at Movie.py
    The beautifulDays function calculates the number of "beautiful days" within a given range [i, j], where a day is considered beautiful if the absolute difference between the day and its reversed form is divisible by
    k. By iterating through the range and applying the condition, the function returns the total count of beautiful days.

---
+++ Implementation/Beautiful Days at Movie.py
@@ -0,0 +1,53 @@
+"""
+Sample Input:
+    20 23 6
+Sample Output:
+    2
+"""
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'beautifulDays' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER i
+#  2. INTEGER j
+#  3. INTEGER k
+#
+
+def beautifulDays(i, j, k):
+    # Write your code here
+    beautiful_number = 0
+    
+    for num in range(i, j+1):
+        reversed_number = ''.join(reversed(str(num)))
+        if abs(num - int(reversed_number))%k == 0:
+            beautiful_number+=1
+    
+    return beautiful_number
+        
+        
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    i = int(first_multiple_input[0])
+
+    j = int(first_multiple_input[1])
+
+    k = int(first_multiple_input[2])
+
+    result = beautifulDays(i, j, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
