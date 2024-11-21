1e4a76a200d34c5da2be9976294bfa7f3d265735
Aadityaganesh <adityaganesh234@gmail.com>
2024-11-21 18:26:50 +0530

    Create Angry Professor.py
    The angryProfessor function determines whether a class is cancelled based on student arrival times and a threshold for attendance. It checks if the number of students arriving on time or early (arrival times ≤ 0) meets the threshold k; if not, the class is cancelled and "YES" is returned; otherwise, "NO" is returned.

---
+++ Implementation/Angry Professor.py
@@ -0,0 +1,53 @@
+"""
+Sample Input
+       2
+case1: 4 3
+       -1 -3 4 2
+case2: 4 2
+       0 -1 2 1
+Sample Output
+case1: YES
+case2: NO
+"""
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'angryProfessor' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. INTEGER k
+#  2. INTEGER_ARRAY a
+#
+
+def angryProfessor(k, a):
+    # Write your code here
+    student_count = sum(1 for i in a if i<=0)
+    return "NO" if student_count >= k else "YES"    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        first_multiple_input = input().rstrip().split()
+
+        n = int(first_multiple_input[0])
+
+        k = int(first_multiple_input[1])
+
+        a = list(map(int, input().rstrip().split()))
+
+        result = angryProfessor(k, a)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
