7bffad19f9effc27ada2edcfbdc9e645759aa5a2
Aadityaganesh <adityaganesh234@gmail.com>
2024-11-25 22:00:57 +0530

    Create Circular Array Rotation.py
    This program circularArrayRotation performs right circular rotations on an array a for a given count k and retrieves values at specified query indices. It uses modular arithmetic to optimize rotations, ensuring efficient computation even for large inputs.

---
+++ Implementation/Circular Array Rotation.py
@@ -0,0 +1,64 @@
+"""
+Sample Input 0:
+    3 2 3
+    1 2 3
+    0
+    1
+    2
+Sample Output 0:
+    2
+    3
+    1
+"""
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'circularArrayRotation' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY a
+#  2. INTEGER k
+#  3. INTEGER_ARRAY queries
+#
+
+def circularArrayRotation(a, k, queries):
+    # Write your code here
+    value = []
+    k = k % n
+    for i in queries:
+        value.append(a[(n - k + i) % n])
+    return value
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    q = int(first_multiple_input[2])
+
+    a = list(map(int, input().rstrip().split()))
+
+    queries = []
+
+    for _ in range(q):
+        queries_item = int(input().strip())
+        queries.append(queries_item)
+
+    result = circularArrayRotation(a, k, queries)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
